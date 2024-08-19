c84cb31f115f932c88b895925a6ca2f21e84881c
binglin <1142983070@qq.com>
2024-08-19 13:20:46 +0800

    tset

--- src/components/Map.vue
+++ src/components/Map.vue
@@ -1,19 +1,89 @@
 <template>
     <div class="map-content">
-        <div id="map-content"></div>
-        <!-- longtitude and latitude -->
-        <div class="long-lat">{{ lonlat[0] }}°E,{{ latLong[1] }}°N</div>
+      <div id="map-content"></div>
+      <!-- 经纬度 -->
+      <div class="map-lat-long" :style="{ right: MAP_TOOLS.right + 40 + 'px' }">{{ latLong[0] }}°E,{{ latLong[1] }}°N</div>
     </div>
-</template>
-
-<script setup lang="ts">
-
-const lonlat = ref([0,0])
- onMounted(async() => {
-    
- }),
-</script>
-
-<style scoped>
-
-</style>
+  </template>
+  <script lang="ts" setup>
+  import { mapInit, showPopupInfo } from "../map/index";
+//   import { mapLayers, popups, searchMapLayer } from "@/function/maps";
+//   import { popupString } from "@/function";
+  import { GlobalModule } from "@/store/modules/global";
+  import { storeToRefs } from "pinia";
+  
+  const latLong = ref([0, 0]);
+  const { setPanelDetails } = GlobalModule();
+  const { MAP_TOOLS, MAP_LOAD, MAP_NAME, MAP } = storeToRefs(GlobalModule());
+  onMounted(async () => {
+    MAP.value = await mapInit({
+      animate: false,
+      mode: (import.meta as { env: { VITE_MODE?: string } }).env.VITE_MODE,
+      baseUrl: (import.meta as { env: { VITE_APP_BASE_API?: string } }).env.VITE_APP_BASE_API,
+      mapDiv: "map-content",
+      token: (import.meta as { env: { VITE_APP_MAP_TOKEN?: string } }).env.VITE_APP_MAP_TOKEN,
+      zoom: 6.1,
+      center: [113.7, 38.8],
+      mousemoveCallback: (e: number[]) => {
+        latLong.value = e;
+      },
+      popupCallback: (layerName: any, code: any, coor: any) => {
+        console.log(layerName, code, coor);
+        try {
+          popups[layerName].api(code).then((res: { data: { [x: string]: any; center?: any; extent?: any } }) => {
+            const h = popupString(popups[layerName].columns, res.data, res.data[popups[layerName].title], null, () => {
+              setPanelDetails({
+                ...popups[layerName].panel,
+                p_title: res.data[popups[layerName].title],
+                ...res.data
+              });
+            });
+            showPopupInfo(h, coor || res.data.center);
+          });
+        } catch (error) {
+          console.log(error);
+        }
+      }
+    });
+    MAP_LOAD.value = true;
+    if (MAP_NAME.value === "HOME") {
+      const data = [...mapLayers.value.xz, ...mapLayers.value.gh];
+      data.forEach((layer) => {
+        if (layer.show) {
+          searchMapLayer(layer.label, true);
+        }
+      });
+    }
+  });
+  onBeforeUnmount(() => {
+    MAP.value.remove();
+    MAP_LOAD.value = false;
+  });
+  </script>
+  <style lang="scss" scoped>
+  .map-content {
+    width: 100%;
+    height: 100%;
+    position: relative;
+  }
+  #map-content {
+    width: 100%;
+    height: 100%;
+  }
+  .map-lat-long {
+    position: absolute;
+    bottom: 20px;
+    right: 80px;
+    background: var(--color--light);
+    padding: 4px 10px;
+    width: 140px;
+    z-index: 0;
+    transition: right 0.3s ease;
+    border-radius: 2px;
+    box-shadow: 0px 2px 4px 0px rgba(0, 0, 0, 0.06);
+    font-family: monospace;
+    color: var(--color);
+    font-size: 16px;
+  }
+  </style>
+  
--- src/store/modules/global.ts
+++ src/store/modules/global.ts
@@ -1,14 +1,59 @@
 import { defineStore } from "pinia";
 
 export interface GlobalState {
-    MAP_TOOLS: { right: number; top: number; yx: boolean; dx: boolean; tl: boolean };
-    MAP?: any;
+  PANEL_SHOW: boolean;
+  MAP_TOOLS: { right: number; top: number; yx: boolean; dx: boolean; tl: boolean };
+  PANEL_DETAILS: { p_key: string | number; p_component: string; p_title: any; p_width: number } | any;
+//   HN_MODEL: string;
+//   HN_MODEL_ITEM: any;
+  MENU_ACTIVE: any;
+//   LEGEND: { show: boolean; type: any; options?: any };
+  MAP_NAME?: string;
+  MAP_LOAD?: boolean;
+  MAP?: any;
 }
 
 export const GlobalModule = defineStore({
-    id: "app-global",
-    state: (): GlobalState => ({
-        MAP_TOOLS: { right: 0, top: 0, yx: false, dx: false, tl: false },
-        MAP: null,
-    })
+  id: "app-global",
+  state: (): GlobalState => ({
+    PANEL_SHOW: false,
+    MAP_TOOLS: { right: 20, top: 200, yx: false, dx: false, tl: false },
+    PANEL_DETAILS: { p_key: "1", p_component: "", p_title: "", p_width: 400 },
+    // HN_MODEL: "",
+    // HN_MODEL_ITEM: {},
+    MENU_ACTIVE: {},
+    // LEGEND: { show: false, type: "" },
+    MAP_NAME: "HOME",
+    MAP_LOAD: false,
+    MAP: null
+  }),
+  getters: {
+    getPanelDetails(state) {
+      return state.PANEL_DETAILS.value.p_component;
+    }
+  },
+  actions: {
+    async setPanelDetails(item: GlobalState["PANEL_DETAILS"] & { key: string | number }, isChangeKey = true) {
+      this.PANEL_SHOW = true;
+      if (isChangeKey) {
+        item.p_key = await guid();
+      } else {
+        item.p_key = item.p_component;
+      }
+      this.PANEL_DETAILS = item;
+    },
+    async mapCompleted() {
+      return new Promise((resolve) => {
+        if (this.MAP_LOAD) {
+          resolve(true);
+        } else {
+          setTimeout(() => {
+            this.mapCompleted().then((res) => {
+              resolve(res);
+            });
+          }, 100);
+        }
+      });
+    }
+  }
 });
